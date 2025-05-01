855d16479df20285fb76734096635de67c44edb5
TomikoMatsuura <tomiko2016@icloud.com>
2025-05-01 21:21:06 +0900

    kadai_005のアップロード②

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,1 +1,13 @@
-console.log(68 /[1.7 * 1.7]);
+// 変数の宣言
+let userName;
+
+// 値の代入
+//userName = 'weight';
+//userName = 'height';
+//userName = 'bmi';
+
+let weight = 68;
+let height = 1.7;
+let bmi = weight / (height * height);
+
+console.log(bmi);
