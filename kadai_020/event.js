1a2037960fa08203ff39ade0dcb1e69bbb98ad74
TomikoMatsuura <tomiko2016@icloud.com>
2025-05-11 18:28:16 +0900

    kadai_020②をアップロード

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,9 +1,10 @@
 // "btn"をいうidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
 
+const text = document.getElementById('text');
 // HTML要素がクリックされた時にイベント処理を実行する
 btn.addEventListener('click', () => {
-     text.innerHTML = 'ボタンをクリックしました';
+     text.textContent = 'ボタンをクリックしました';
 });
   
  
